06a0c51dc8aab4336ce752b10450e1cfe6be2669
Evgeniya Sheveleva <sheveleva.evg@gmail.com>
2021-04-13 23:56:29 +0300

    rebase

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -3,8 +3,14 @@
 
 count=1
 for param in "$@"; do
+<<<<<<< HEAD
     echo "\$@ Parameter #$count = $param"
     count=$(( $count + 1 ))
 done
 
 echo "====="
+=======
+    echo "Next parameter: $param"
+    count=$(( $count + 1 ))
+done
+>>>>>>> git-rebase
